1d6eb0860bbbac9c198bba81515175c5bcb3887c
Santosh(Delhi) <santoshpsd13@gmail.com>
2017-07-19 17:13:56 +0530

    finishing the project

--- scripts/main.js
+++ scripts/main.js
@@ -24,18 +24,13 @@ var simonGame = {
 			//console.log("PATEERN: " + simonGame.PATTERN + "COUNT " + simonGame.COUNT );
 			//calling user input
 			console.log("calling user Input");
-			var result = handler.userInput();console.log("result " + result.userInput);
-			if (result.userInput == "wrong") {
-				handler.patternRepeatPlayer();
-			} else if (result.userInput == "success"){
-				setTimeout(function(){
+			handler.userInput();
+			setTimeout(function(){
 				if(handler.repeatFlag === false){  //execute count gen only if repeat flag is false inside user INPUT
 					genPattern();
 				}
 			},simonGame.COUNT*2000);
-			} else if(result.userInput == "empty"){
-				console.log(" No Response from User ");
-			}
+		 
 			//console.log("pattern check true, calling pattern gen");
 			
 		//},simonGame.COUNT*5000); //close setTimeout
@@ -48,14 +43,16 @@ var simonGame = {
 
 var handler = {
 	countRepPlayer: 0,
-	userPattern: [],
+	repeatFlag: false,
 	patternRepeatPlayer: function(){
 		var repeater = setInterval(function(){
 				handler.effect(simonGame.PATTERN[handler.countRepPlayer]);
 				handler.countRepPlayer += 1;
 				if(handler.countRepPlayer > simonGame.COUNT){
 					clearInterval(repeater);
-					setTimeout(function(){simonGame.patternMatcher();},1000);
+					//setTimeout(function(){
+						simonGame.patternMatcher();
+						//},1000);
 					handler.countRepPlayer = 0;
 				}
 			},1000);//close sestInterval
@@ -74,18 +71,12 @@ var handler = {
 	countDisplay: function(){
 		document.getElementById("count").innerHTML = "Count: " + simonGame.COUNT;
 	}, //close countIncrease
-	getUserInput: function(){
-		//var userPattern = new Array();
-		 $('img').click(function(){
-				//console.log("Image Clicked by User " + this.id + simonGame.PATTERN);userPattern.push(this.id);
-				//console.log("userPattern.indexOf(this.id) !== simonGame.PATEERN(this.id) " + userPattern.indexOf(this.id) !== simonGame.PATTERN.indexOf(this.id));			
-				
-				var id = parseInt(this.id,10);
-				handler.userPattern.push(id);
+	userInput: function(){
+		var userPattern = new Array();var id;
+		 $('img').click(function(){						
+				id = parseInt(this.id,10);
+				userPattern.push(id);
 				handler.effect(id);
-				});
-	}
-	checkUserInput: function(){
 				console.log(" user " + userPattern); 
 				console.log(" pattern " + simonGame.PATTERN);
 				if(userPattern.indexOf(id) !== simonGame.PATTERN.indexOf(id)){
@@ -99,22 +90,17 @@ var handler = {
 						var audio = new Audio('sounds/wrong.mp3');
 						audio.play();
 						userPattern.length = 0;
-						return {userInput:"wrong"};
-						
+						handler.repeatFlag = true;
+						handler.patternRepeatPlayer();
+						return ;
 					}
 				}
-				if(userPattern.length === 0){
-					return {userInput: "empty"};
-				}
+				//reset the userPattern Array
 				if(userPattern.length === simonGame.PATTERN.length){
 					userPattern.length = 0;
-					return {userInput: "success"};
 				}
-				
-				//console.log(" Index Of this.id inside userPattern " + userPattern.indexOf(this.id));
-				//console.log(" Index Of this.id inside PATTERN " + simonGame.PATTERN.indexOf(this.id));	
-			
-		
+		});		//close click.
+					
 	}
 	
 	
