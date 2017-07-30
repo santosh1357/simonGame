83eb2bb754a1df047fa8e5dbcbcef2e38446df2a
Santosh(Delhi) <santoshpsd13@gmail.com>
2017-07-30 22:20:29 +0530

    done

--- scripts/main.js
+++ scripts/main.js
@@ -2,51 +2,33 @@ var simonGame = {
 	COUNT: 0,
 	PATTERN: [],
 	SOUND:[{file:'sounds/sa.mp3'},{file:'sounds/re.mp3'},{file:'sounds/ga.mp3'},{file:'sounds/ma.mp3'},{file:'sounds/pa.mp3'},{file:'sounds/dha.mp3'},{file:'sounds/nee.mp3'}],
-	patternGen: function(){
+	patternGen: function(param){
 		var randomId;
+		if(param == undefined){ 
+			param = {result:'fail'};
+		}
 		randomId = Math.floor(Math.random() * 7);
 		simonGame.PATTERN.push(randomId);
 		if(simonGame.COUNT > 20){
 			alert("You have won the game!!");
 			window.location.reload(true);
+		} else if(simonGame.COUNT === 0 || param.result==='success'){
+			simonGame.COUNT += 1;
+			handler.countDisplay();
+			handler.patternRepeatPlayer();	
+		} else {
+			window.location.reload(true);
 		}
-		simonGame.COUNT += 1;
-		//debugger;
-		//console.log("increase count true calling count display " + simonGame.COUNT);
-		handler.countDisplay();
-		//console.log("count gen true calling patternPlayer with PATTERN " + simonGame.PATTERN );
-		handler.patternRepeatPlayer();
-	}, //close patternGen
-	patternMatcher: function(genPattern){
-		//console.log("inside patternMatch");
-		var genPattern = simonGame.patternGen;
-			//calling user input
-			console.log("calling user Input");
-			handler.userInput();
-			setTimeout(function(){
-				if(handler.repeatFlag === false){  //execute count gen only if repeat flag is false inside user INPUT
-					console.log("calling pattern Gen");
-					genPattern();
-				}
-			},simonGame.COUNT*2000);
-	}, //close patternMatcher
-
+	} //close patternGen
 } //close simonGame
+	
+
+
+
 
 var handler = {
 	countRepPlayer: 0,
-	repeatFlag: false,
-	patternRepeatPlayer: function(){
-		var repeater = setInterval(function(){
-				handler.effect(simonGame.PATTERN[handler.countRepPlayer]);
-				handler.countRepPlayer += 1;
-				if(handler.countRepPlayer > simonGame.COUNT){ //If all ids inside pattern has been played, clearInterval, reset repeat count and call patternMatch.
-					clearInterval(repeater);
-						handler.countRepPlayer = 0;
-						simonGame.patternMatcher();
-				}
-			},1000);//close sestInterval
-	}, //close patternRepeatPlayer
+	divs:[0,1,2,3,4,5,6],
 	effect: function(id){
 	   var img = document.getElementById(id);
 	   if(img !== null && id !== undefined){
@@ -61,39 +43,67 @@ var handler = {
 		document.getElementById("count").innerHTML = "Count: " + simonGame.COUNT;
 	}, //close countIncrease
 	userInput: function(){
-		var userPattern = new Array();var id;
-		 $('img').click(function(e){						
-				id = parseInt(this.id,10);
-				userPattern.push(id);
-				handler.effect(id);
-				console.log(" user " + userPattern); 
-				console.log(" pattern " + simonGame.PATTERN);
-				if(userPattern.indexOf(id) !== simonGame.PATTERN.indexOf(id)){
-					console.log(" WRONG USER INPUT ");
+		var userPattern = new Array();var id;var success = true;
+		 function handleWrongInput(){
+			console.log(" WRONG USER INPUT ");
 					if($('.chkStrict:checked').val() === "on"){
 						var audio = new Audio('sounds/wrong.mp3');
 						audio.play();
 						setTimeout(function(){window.location.reload(true)},1000);
 					} else {
 						console.log("inside else " );
-						debugger;
+						//debugger;
 						var audio = new Audio('sounds/wrong.mp3');
 						audio.play();
-						userPattern.length = 0;
-						handler.repeatFlag = true;
-						handler.patternRepeatPlayer(); //this is getting called recursivelly rather	 
-						return ;
+						setTimeout(function(){handler.patternRepeatPlayer()},1200); //this is getting called recursivelly rather	 
+						 
+					}
+		}
+		console.log("Isde user in");
+		handler.divs.forEach(function(div){	
+				var img = document.getElementById(div);
+				img.onclick = function(){
+					var id = parseInt(img.id,10);
+					userPattern.push(id);
+					handler.effect(id);
+					console.log(" div clicked " + id + " u p " + userPattern);
+					if(userPattern.indexOf(id) !== simonGame.PATTERN.indexOf(id)){
+						console.log(" if ");
+						handleWrongInput();
+					} else if(userPattern.length === simonGame.PATTERN.length){
+						setTimeout(function(){simonGame.patternGen({result:"success"})},1200);
 					}
 				}
-				//reset the userPattern Array
-				if(userPattern.length === simonGame.PATTERN.length){
-					userPattern.length = 0;
+				
+		});	console.log(" up " + userPattern + " SGP " + simonGame.PATTERN);
+		
+	},
+	patternRepeatPlayer: function(param){
+		var repeater = setInterval(function(){
+				handler.effect(simonGame.PATTERN[handler.countRepPlayer]);
+				handler.countRepPlayer += 1;
+				if(handler.countRepPlayer > simonGame.COUNT){ //If all ids inside pattern has been played, clearInterval, reset repeat count and call patternMatch.
+					clearInterval(repeater);
+						handler.countRepPlayer = 0;
+						handler.userInput();
 				}
-				e.stopImmediatePropagation(); 
-				e.preventDefault();
-		});		//close click.
-					
-	}
-	
-	
-} //close handler
+			},1000);//close sestInterval
+	} //close patternRepeatPlayer
+
+}	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
